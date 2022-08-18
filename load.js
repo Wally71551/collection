cc6c19bcbbd5fd6ecaa1e6abd603dfcac34dc857
BenThompson-Dev <111389603+Wally71551@users.noreply.github.com>
2022-08-18 03:28:54 +0100

    Updated formatting on images

--- load.js
+++ load.js
@@ -14,7 +14,51 @@ $.ajax({
         }
         $.each(data, function (key, value) {
             console.log(value.title);
+
+            let itemInfo = value;
+            let itemInfoDiv = document.createElement("div")
+            itemInfoDiv.classList.add("name-item");
+
+            let itemTitle = document.createElement("h3");
+            let itemTitleNode = document.createTextNode(itemInfo.title);
+            itemTitle.appendChild(itemTitleNode);
+            itemTitle.classList.add("title");
+
+            let itemType = document.createElement("h4");
+            let itemTypeNode = document.createTextNode(itemInfo.type)
+            itemType.appendChild(itemTypeNode);
+            itemType.classList.add("type");
+
+            let itemPlatform = document.createElement("p");
+            let itemPlatformNode = document.createTextNode(itemInfo.platform);
+            itemPlatform.appendChild(itemPlatformNode);
+            itemPlatform.classList.add("platform");
+
+            let itemStorefront = document.createElement("p");
+            let itemStorefrontNode = document.createTextNode(itemInfo.storefront);
+            itemStorefront.appendChild(itemStorefrontNode);
+            itemStorefront.classList.add("storefront");
+
+            //Sets up the basic display element
+            itemInfoDiv.appendChild(itemTitle);
+            itemInfoDiv.appendChild(itemType);
+            itemInfoDiv.appendChild(itemPlatform);
+            itemInfoDiv.appendChild(itemStorefront);
+
+            if (itemInfo.image != null) {
+                let itemImage = document.createElement("img");
+                itemImage.src = itemInfo.image;
+                itemImage.classList.add("image");
+
+                itemInfoDiv.appendChild(itemImage);
+            }
+
+            collectionElement.appendChild(itemInfoDiv);
         });
+
+        loaderElement.style.display = "none";
+        collectionElement.style.display = "block";
+        errorElement.style.display = "none";
     })
     .fail(function () {
         console.log("Failed");
