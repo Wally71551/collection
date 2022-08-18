4ba71d6bc871cbdd94cc84f7016d84d39864cc71
BenThompson-Dev <111389603+Wally71551@users.noreply.github.com>
2022-08-18 03:53:44 +0100

    Reformatted loading to format the display easier

--- load.js
+++ load.js
@@ -15,45 +15,7 @@ $.ajax({
         $.each(data, function (key, value) {
             console.log(value.title);
 
-            let itemInfo = value;
-            let itemInfoDiv = document.createElement("div")
-            itemInfoDiv.classList.add("name-item");
-
-            let itemTitle = document.createElement("h3");
-            let itemTitleNode = document.createTextNode(itemInfo.title);
-            itemTitle.appendChild(itemTitleNode);
-            itemTitle.classList.add("title");
-
-            let itemType = document.createElement("h4");
-            let itemTypeNode = document.createTextNode(itemInfo.type)
-            itemType.appendChild(itemTypeNode);
-            itemType.classList.add("type");
-
-            let itemPlatform = document.createElement("p");
-            let itemPlatformNode = document.createTextNode(itemInfo.platform);
-            itemPlatform.appendChild(itemPlatformNode);
-            itemPlatform.classList.add("platform");
-
-            let itemStorefront = document.createElement("p");
-            let itemStorefrontNode = document.createTextNode(itemInfo.storefront);
-            itemStorefront.appendChild(itemStorefrontNode);
-            itemStorefront.classList.add("storefront");
-
-            //Sets up the basic display element
-            itemInfoDiv.appendChild(itemTitle);
-            itemInfoDiv.appendChild(itemType);
-            itemInfoDiv.appendChild(itemPlatform);
-            itemInfoDiv.appendChild(itemStorefront);
-
-            if (itemInfo.image != null) {
-                let itemImage = document.createElement("img");
-                itemImage.src = itemInfo.image;
-                itemImage.classList.add("image");
-
-                itemInfoDiv.appendChild(itemImage);
-            }
-
-            collectionElement.appendChild(itemInfoDiv);
+            CreateItem(value);
         });
 
         loaderElement.style.display = "none";
@@ -69,4 +31,48 @@ function SetErrorDisplay() {
     loaderElement.style.display = "none"
     collectionElement.style.display = "none"
     errorElement.style.display = "block"
+}
+
+//Creates the item display
+function CreateItem(itemInfo) {
+    let itemInfoDiv = document.createElement("div")
+    itemInfoDiv.classList.add("name-item");
+
+    let itemTitle = document.createElement("h3");
+    let itemTitleNode = document.createTextNode(itemInfo.title);
+    itemTitle.appendChild(itemTitleNode);
+    itemTitle.classList.add("title");
+    itemInfoDiv.appendChild(itemTitle);
+
+    let itemType = document.createElement("h4");
+    let itemTypeNode = document.createTextNode(itemInfo.type)
+    itemType.appendChild(itemTypeNode);
+    itemType.classList.add("type");
+    itemInfoDiv.appendChild(itemType);
+
+    if (itemInfo.platform != null) {
+        let itemPlatform = document.createElement("p");
+        let itemPlatformNode = document.createTextNode(itemInfo.platform);
+        itemPlatform.appendChild(itemPlatformNode);
+        itemPlatform.classList.add("platform");
+        itemInfoDiv.appendChild(itemPlatform);
+    }
+
+    if (itemInfo.storefront != null) {
+        let itemStorefront = document.createElement("p");
+        let itemStorefrontNode = document.createTextNode(itemInfo.storefront);
+        itemStorefront.appendChild(itemStorefrontNode);
+        itemStorefront.classList.add("storefront");
+        itemInfoDiv.appendChild(itemStorefront);
+    }
+
+    if (itemInfo.image != null) {
+        let itemImage = document.createElement("img");
+        itemImage.src = itemInfo.image;
+        itemImage.classList.add("image");
+
+        itemInfoDiv.appendChild(itemImage);
+    }
+
+    collectionElement.appendChild(itemInfoDiv);
 }
