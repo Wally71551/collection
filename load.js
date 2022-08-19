1d3ad029178e418855a70514a3189217491f22e6
BenThompson-Dev <111389603+Wally71551@users.noreply.github.com>
2022-08-19 18:01:10 +0100

    DLC / title display tweaks

--- load.js
+++ load.js
@@ -1,3 +1,5 @@
+//Handles loading the main collection page
+
 let collectionElement = document.getElementById("collection");
 let loaderElement = document.getElementById("loader");
 let errorElement = document.getElementById("error");
@@ -26,15 +28,16 @@ $.ajax({
         SetErrorDisplay();
     });
 
+//Shows an error message
 function SetErrorDisplay() {
     loaderElement.style.display = "none"
     collectionElement.style.display = "none"
     errorElement.style.display = "block"
 }
 
-//Creates the item display
+//Function creates an individual item display based on the spreadsheet row passed in
 function CreateItem(itemInfo) {
-    //Create list item (for base)
+    //Creates the base box
     let itemInfoDiv = document.createElement("li")
     itemInfoDiv.classList.add("name-item");
 
@@ -62,11 +65,48 @@ function CreateItem(itemInfo) {
     itemImageWrapper.appendChild(itemImage);
     itemInfoDiv.appendChild(itemImageWrapper);
 
+    //Checks to see if the item is DLC and adjusts how the name is displayed
+    if (itemInfo.type == "DLC") {
+        itemInfo.linkedtitles += " "
+        itemInfo.title = itemInfo.title.replace(itemInfo.linkedtitles, "");
+        console.log(itemInfo.title);
+
+        itemInfo.subtitle = itemInfo.linkedtitles;
+    }
+
     let itemTitle = document.createElement("h3");
     let itemTitleNode = document.createTextNode(itemInfo.title);
     itemTitle.appendChild(itemTitleNode);
     itemTitle.classList.add("title");
     itemInfoDiv.appendChild(itemTitle);
+
+    //Decides the colour of the box
+    if (itemInfo.playing == "1") {
+        itemTitle.classList.add("playing-item");
+    }
+    else if (itemInfo.completed == "1") {
+        itemTitle.classList.add("completed-item");
+    }
+    else if (itemInfo.unplayed == "1") {
+        itemTitle.classList.add("unplayed-item");
+    }
+    else if (itemInfo.replay == "1") {
+        itemTitle.classList.add("replay-item");
+    }
+    else if (itemInfo.retired == "1") {
+        itemTitle.classlist.add("retired-item");
+    }
+    else {
+        itemTitle.classList.add("backlog-item");
+    }
+
+    if (itemInfo.subtitle != null) {
+        let itemSubtitle = document.createElement("p");
+        let itemSubtitleNode = document.createTextNode(itemInfo.subtitle);
+        itemSubtitle.appendChild(itemSubtitleNode);
+        itemSubtitle.classList.add("subtitle");
+        itemInfoDiv.appendChild(itemSubtitle);
+    }
 
     let itemType = document.createElement("h4");
     let itemTypeNode = document.createTextNode(itemInfo.type)
